297d6e39b09923e7fe67ee3bbaccc6db71f3526a
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-04-10 19:50:02 +0200

    Fix test initializer

--- Tests/SwiftRexTests/Mocks/Store/TestStore.swift
+++ Tests/SwiftRexTests/Mocks/Store/TestStore.swift
@@ -3,7 +3,7 @@ import RxSwift
 import XCTest
 
 final class TestStore: StoreBase<TestState> {
-    required init<M>(initialState: E, reducer: Reducer<E>, middleware: M) where E == M.StateType, M: Middleware {
+    override init<M>(initialState: E, reducer: Reducer<E>, middleware: M) where E == M.StateType, M: Middleware {
         super.init(initialState: initialState, reducer: reducer, middleware: middleware)
     }
 }
