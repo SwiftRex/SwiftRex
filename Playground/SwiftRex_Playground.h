5e5f06097f33d71aaa3e08383d17f3acac6be803
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-04-15 18:38:35 +0200

    Playground

---
+++ Playground/SwiftRex_Playground.h
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftRex_Playground.
+FOUNDATION_EXPORT double SwiftRex_PlaygroundVersionNumber;
+
+//! Project version string for SwiftRex_Playground.
+FOUNDATION_EXPORT const unsigned char SwiftRex_PlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRex_Playground/PublicHeader.h>
+
+
