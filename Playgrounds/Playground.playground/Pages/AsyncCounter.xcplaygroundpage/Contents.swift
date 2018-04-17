02234f2117e20871cebb4850b8cd6a727dc5ddb1
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-04-18 00:44:47 +0200

    Improve async counter playground

--- Playgrounds/Playground.playground/Pages/AsyncCounter.xcplaygroundpage/Contents.swift
+++ Playgrounds/Playground.playground/Pages/AsyncCounter.xcplaygroundpage/Contents.swift
@@ -36,7 +36,8 @@ struct AlertMessage: Equatable {
  Events that users can send
  */
 enum CounterEvent: EventProtocol, Equatable {
-    case didTapIncrease, didTapDecrease
+    case didTapIncrease
+    case didTapDecrease
 }
 
 /*******************************************************************************
@@ -72,17 +73,18 @@ final class CounterService: SideEffectProducer {
     }
 
     func execute(getState: @escaping () -> GlobalState) -> Observable<ActionProtocol> {
-        switch event {
-        case .didTapIncrease:
-            guard case .stopped = getState().countingState.requestState else {
-                return .just(AlertAction.error(Date(), "Can't increase: another pending operation"))
-            }
+        // Here we guard against triggering a request when other is
+        // currently happening. In a real-life scenario we could instead
+        // cancel the pending request or simply ignore
+        switch (event, getState().countingState.requestState) {
+        case (.didTapIncrease, .stopped):
             return increase().map { $0 as ActionProtocol }
-        case .didTapDecrease:
-            guard case .stopped = getState().countingState.requestState else {
-                return .just(AlertAction.error(Date(), "Can't decrease: another pending operation"))
-            }
+        case (.didTapDecrease, .stopped):
             return decrease().map { $0 as ActionProtocol }
+        case (.didTapIncrease, _):
+            return .just(AlertAction.error(Date(), "Can't increase: another pending operation"))
+        case (.didTapDecrease, _):
+            return .just(AlertAction.error(Date(), "Can't decrease: another pending operation"))
         }
     }
 
@@ -209,6 +211,39 @@ let store = Store()
 
 
 /*******************************************************************************
+ ViewModel
+ *******************************************************************************
+
+ Struct that represents the mutable properties of a View
+ */
+struct CounterViewModel: CustomDebugStringConvertible {
+    let emoji: String
+    let title: String
+    let details: String
+
+    init(state: CountingState) {
+        switch state.requestState {
+        case .stopped:
+            emoji = "✅"
+            title = "Value"
+            details = String(state.currentNumber)
+        case .requestingToIncrease:
+            emoji = "⏳"
+            title = "Increasing"
+            details = "Increasing: \(state.currentNumber) => \(state.currentNumber + 1)"
+        case .requestingToDecrease:
+            emoji = "⏳"
+            title = "Decreasing"
+            details = "Decreasing: \(state.currentNumber) => \(state.currentNumber - 1)"
+        }
+    }
+
+    var debugDescription: String {
+        return "\(emoji)\t| \(title): \(details)"
+    }
+}
+
+/*******************************************************************************
  ViewController
  *******************************************************************************
 
@@ -219,42 +254,64 @@ let store = Store()
  */
 
 let disposeBag = DisposeBag()
+let buttonIncrease = PublishSubject<Void>()
+let buttonDecrease = PublishSubject<Void>()
 
-// Subscription to present the state
-store
-    .map { $0.countingState }
-    .distinctUntilChanged()
-    .subscribe(onNext: {
-        switch $0.requestState {
-        case .stopped:
-            print("✅\t| Value: \($0.currentNumber)")
-        case .requestingToIncrease:
-            print("⏳\t| Increasing: \($0.currentNumber) => \($0.currentNumber + 1)")
-        case .requestingToDecrease:
-            print("⏳\t| Decreasing: \($0.currentNumber) => \($0.currentNumber - 1)")
-        }
-    }).disposed(by: disposeBag)
+func viewDidLoad() {
+    // Subscription to present the state
+    store[\.countingState]
+        .distinctUntilChanged()
+        .map(CounterViewModel.init)
+        .subscribe(onNext: update)
+        .disposed(by: disposeBag)
 
-// Subscription to present errors on the screen
-store
-    .map { $0.lastMessage }
-    .distinctUntilChanged { $0.date }
-    .filter { $0.text != nil }
-    .map { $0.text! }
-    .subscribe(onNext: { message in
-        print("❌\t| \(message)")
-    }).disposed(by: disposeBag)
+    // Subscription to present errors on the screen
+    store[\.lastMessage]
+        .distinctUntilChanged(\.date)
+        .map(\.text)
+        .unwrap()
+        .map { "❌\t| \($0)" }
+        .subscribe(onNext: update)
+        .disposed(by: disposeBag)
 
+    // Bind UIButton `buttonIncrease` touchUpInside to CounterEvent.didTapIncrease
+    buttonIncrease
+        .mapTo(CounterEvent.didTapIncrease)
+        .subscribe(onNext: store.dispatch)
+        .disposed(by: disposeBag)
 
-// Let's simulate some button taps
+    // Bind UIButton `buttonDecrease` touchUpInside to CounterEvent.didTapDecrease
+    buttonDecrease
+        .mapTo(CounterEvent.didTapDecrease)
+        .subscribe(onNext: store.dispatch)
+        .disposed(by: disposeBag)
+}
+
+func update(_ value: CustomDebugStringConvertible) {
+    print(value)
+}
+
+viewDidLoad()
+
+//
+//
+
+/*******************************************************************************
+ User interaction
+ *******************************************************************************
+
+ Let's simulate some button taps
+ */
+
 let events = [
-    CounterEvent.didTapIncrease,
-    CounterEvent.didTapIncrease,
-    CounterEvent.didTapIncrease,
-    CounterEvent.didTapDecrease,
-    CounterEvent.didTapIncrease,
-    CounterEvent.didTapDecrease,
-    CounterEvent.didTapDecrease]
+    buttonIncrease.onNext,
+    buttonIncrease.onNext,
+    buttonIncrease.onNext,
+    buttonDecrease.onNext,
+    buttonIncrease.onNext,
+    buttonDecrease.onNext,
+    buttonDecrease.onNext
+]
 
 // Interval between taps
 // Setting to 0.55, which is very close to the time needed to
@@ -265,7 +322,7 @@ events
     .enumerated()
     .map { pair in
         (time: DispatchTime.now() + Double(pair.0) * interval,
-         work: { store.dispatch(pair.1) }) }
+         work: pair.1) }
     .forEach {
         DispatchQueue.main.asyncAfter(deadline: $0.time, execute: $0.work)
     }
@@ -292,3 +349,27 @@ events
  `state.countingState.isLoading` to the button, making it disabled,
  or perhaps cancel the current request and make another one.
 */
+
+
+// Useful extensions
+extension ObservableType {
+    public func unwrap<T>() -> Observable<T> where E == T? {
+        return self.filter { $0 != nil }.map { $0! }
+    }
+
+    public subscript<T>(_ keyPath: KeyPath<E, T>) -> Observable<T> {
+        return self.map { $0[keyPath: keyPath] }
+    }
+
+    public func map<T>(_ keyPath: KeyPath<E, T>) -> Observable<T> {
+        return self.map { $0[keyPath: keyPath] }
+    }
+
+    public func mapTo<T>(_ value: T) -> Observable<T> {
+        return self.map { _ in value }
+    }
+
+    public func distinctUntilChanged<T>(_ keyPath: KeyPath<E, T>) -> Observable<E> where T: Equatable {
+        return self.distinctUntilChanged { $0[keyPath: keyPath] == $1[keyPath: keyPath] }
+    }
+}
