1cd78188ddc5254f1d9df0cd562070a639faeeef
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-04-20 23:21:27 +0200

    Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "SwiftRex",
+    products: [
+        .library(name: "SwiftRex", targets: ["SwiftRex"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "7.1.1"))
+    ],
+    targets: [
+        .target(name: "SwiftRex", dependencies: ["RxSwift"], path: "Sources"),
+        .testTarget(name: "UnitTests", dependencies: ["SwiftRex", "Nimble", "RxTest", "RxBlocking", "RxSwift"], path: "UnitTests")
+    ]
+)
