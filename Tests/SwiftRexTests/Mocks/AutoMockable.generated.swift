bc2d66d188fd5c4cbb1768f7b8438a7f3bee049a
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-04-20 20:54:36 +0200

    Run Sourcery and Lint for all targets

---
+++ Tests/SwiftRexTests/Mocks/AutoMockable.generated.swift
@@ -0,0 +1,49 @@
+// Generated using Sourcery 0.11.2 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+import Foundation
+import RxSwift
+@testable import SwiftRex
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+#elseif os(OSX)
+import AppKit
+#endif
+
+class ActionHandlerMock: ActionHandler {
+
+    // MARK: - trigger
+
+    var triggerCallsCount = 0
+    var triggerCalled: Bool {
+        return triggerCallsCount > 0
+    }
+    var triggerReceivedAction: ActionProtocol?
+    var triggerClosure: ((ActionProtocol) -> Void)?
+
+    func trigger(_ action: ActionProtocol) {
+        triggerCallsCount += 1
+        triggerReceivedAction = action
+        triggerClosure?(action)
+    }
+
+}
+class SideEffectProducerMock: SideEffectProducer {
+
+    // MARK: - execute
+
+    var executeGetStateCallsCount = 0
+    var executeGetStateCalled: Bool {
+        return executeGetStateCallsCount > 0
+    }
+    var executeGetStateReceivedGetState: GetState<StateType>?
+    var executeGetStateReturnValue: Observable<ActionProtocol>!
+    var executeGetStateClosure: ((@escaping GetState<StateType>) -> Observable<ActionProtocol>)?
+
+    func execute(getState: @escaping GetState<StateType>) -> Observable<ActionProtocol> {
+        executeGetStateCallsCount += 1
+        executeGetStateReceivedGetState = getState
+        return executeGetStateClosure.map({ $0(getState) }) ?? executeGetStateReturnValue
+    }
+
+}
