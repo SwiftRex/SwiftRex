6a1b9ef141e394516fa4f293da88763046ecf91e
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-04-23 01:04:54 +0200

    Improve docs

--- Sources/Store/Store.swift
+++ Sources/Store/Store.swift
@@ -5,6 +5,14 @@
 
  The store will glue all the parts together and its responsibility is being a proxy to the non-Redux world. For that reason, it's correct to say that a `Store` is the single point of contact with `UIKit` and it's a class that you want to inject as a dependency on all the ViewControllers, either as one single dependency or, preferably, a dependency for each of its protocols - `EventHandler` and `StateProvider` -, both eventually pointing to the same instance.
 
+ ![Store and ViewController](https://luizmb.github.io/SwiftRex/markdown/img/StoreBase.png)
+
+ In its documentation, Apple suggests some communication patterns between the MVC layers. Most important, they say that Controllers should update the Model, who notifies the Controller about changes:
+
+ ![iOS MVC](https://luizmb.github.io/SwiftRex/markdown/img/CocoaMVC.gif)
+
+ You can think of Store as a very heavy "Model" layer, completely detached from the View and Controller, and where all the business logic stands. At a first sight it may look like transfering the "Massive" problem from a layer to another, but later in this docs it's gonna be clear how the logic will be split and, hopefully, by having specialized middlewares we can even start sharing more code between different apps or different devices such as Apple TV, macOS, iOS, watchOS or backend APIs, thanks to the business decisions being completely off your presentation layer.
+
  You want only one Store in your app, so either you create a singleton or a public property in a long-life class such as AppDelegate or AppCoordinator. That's crucial for making the store completely detached from the `UIKit` world. Theoretically it should be possible to keep multiple stores - one per module or per `UIViewController` - and keep them in sync through Rx observation, like the "Flux" approach. However, the main goal of SwiftRex is to keep an unified state independent from `UIKit`, therefore it's the recommended approach.
 
  Specific implementations of a store will require mechanisms to distribute events, mutate the state and notifies subscribers, probably by using `Middleware` and being an `ActionHandler` too, but this is not necessary for the outside world, that only needs the input and output bus of a store.
