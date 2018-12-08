39f8bb6298a5933ae3913d37ec3d6ee9e2a128c7
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-12-08 23:18:25 +0100

    Commit Pods folder in repo

---
+++ Pods/Target Support Files/Pods-SwiftRex iOS/Pods-SwiftRex iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftRex_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftRex_iOS
+@end
---
+++ Pods/Target Support Files/Pods-SwiftRex tvOS/Pods-SwiftRex tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftRex_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftRex_tvOS
+@end
