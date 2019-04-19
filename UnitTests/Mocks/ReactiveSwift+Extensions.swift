9dad815b7cd4e9dd0a81477623ff876f34a741ca
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-04-19 11:36:12 +0200

    Update Result usage

--- UnitTests/Mocks/ReactiveSwift+Extensions.swift
+++ UnitTests/Mocks/ReactiveSwift+Extensions.swift
@@ -1,17 +1,16 @@
 import struct ReactiveSwift.SignalProducer
 import class ReactiveSwift.ScopedDisposable
 import class ReactiveSwift.CompositeDisposable
-import struct Result.AnyError
 import SwiftRex
 
-func observable<T>(of type: T.Type, error: Swift.Error) -> SignalProducer<T, AnyError> {
-    return SignalProducer<T, AnyError> { observer, _ in
+func observable<T>(of type: T.Type, error: Error) -> SignalProducer<T, Error> {
+    return SignalProducer<T, Error> { observer, _ in
         observer.send(error: .init(error))
     }
 }
 
-func observable<T>(of values: T...) -> SignalProducer<T, AnyError> {
-    return SignalProducer<T, AnyError> { observer, _ in
+func observable<T>(of values: T...) -> SignalProducer<T, Error> {
+    return SignalProducer<T, Error> { observer, _ in
         values.forEach(observer.send)
         observer.sendCompleted()
     }
