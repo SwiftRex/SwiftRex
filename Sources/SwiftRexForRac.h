f07635c8f96a85c395e8b591bb06c899f2fd9b92
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-06-19 01:12:21 +0200

    Projects references between SwiftRex and Rx/Rac

---
+++ Sources/SwiftRexForRac.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftRexForRac.
+FOUNDATION_EXPORT double SwiftRexForRacVersionNumber;
+
+//! Project version string for SwiftRexForRac.
+FOUNDATION_EXPORT const unsigned char SwiftRexForRacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRexForRac/PublicHeader.h>
+
+
