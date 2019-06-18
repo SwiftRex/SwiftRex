0793ea80a3cdf05373217ed2cb7e21e49e6e994c
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-06-18 21:22:11 +0200

    Include SideEffectMiddleware tests

--- UnitTests/Common/Middleware/SideEffectMiddlewareTests.swift
+++ UnitTests/Common/Middleware/SideEffectMiddlewareTests.swift
@@ -22,117 +22,127 @@ class SideEffectMiddlewareTests: MiddlewareTestsBase {
         XCTAssertEqual(0, messageHandler.actionHandlerMock.actions.count)
     }
 
-//    func testSideEffectMiddlewareEvent() {
-//        // Given
-//        let sut = SideEffectMiddlewareMock()
-//
-//        let actionHandler = ActionHandlerMock()
-//        sut.actionHandler = actionHandler
-//        let state = TestState()
-//        let getState = { state }
-//        let event = EventReference()
-//        let lastInChainWasCalledExpectation = self.expectation(description: "last in chain was called")
-//        let lastInChain = lastEventInChain(event, state: state, expectation: lastInChainWasCalledExpectation)
-//        let sideEffect = SideEffectProducerMock()
-//        sideEffect.executeGetStateReturnValue = observable(of: Action1(), Action2(), Action3())
-//        sut.sideEffectForReturnValue = AnySideEffectProducer(sideEffect)
-//        sut.underlyingSubscriptionOwner = SubscriptionOwner.new()
-//        sut.underlyingAllowEventToPropagate = true
-//        var actionsCalled: [ActionProtocol] = []
-//        actionHandler.triggerClosure = { action in actionsCalled.append(action) }
-//
-//        // Then
-//        sut.handle(event: event, getState: getState, next: lastInChain)
-//
-//        // Expect
-//        wait(for: [lastInChainWasCalledExpectation], timeout: 3)
-//        XCTAssertEqual(3, actionHandler.triggerCallsCount)
-//        XCTAssertEqual("a1", (actionsCalled[0] as! Action1).name)
-//        XCTAssertEqual("a2", (actionsCalled[1] as! Action2).name)
-//        XCTAssertEqual("a3", (actionsCalled[2] as! Action3).name)
-//    }
-//
-//    func testSideEffectMiddlewareEventNoPropagation() {
-//        // Given
-//        let sut = SideEffectMiddlewareMock()
-//
-//        let actionHandler = ActionHandlerMock()
-//        sut.actionHandler = actionHandler
-//        let state = TestState()
-//        let getState = { state }
-//        let event = EventReference()
-//        let sideEffect = SideEffectProducerMock()
-//        sideEffect.executeGetStateReturnValue = observable(of: Action1(), Action2(), Action3())
-//        sut.sideEffectForReturnValue = AnySideEffectProducer(sideEffect)
-//        sut.underlyingSubscriptionOwner = SubscriptionOwner.new()
-//        sut.underlyingAllowEventToPropagate = false
-//        var actionsCalled: [ActionProtocol] = []
-//        actionHandler.triggerClosure = { action in actionsCalled.append(action) }
-//
-//        // Then
-//        sut.handle(event: event, getState: getState, next: { _, _ in XCTFail("It should be propagating") })
-//
-//        // Expect
-//        XCTAssertEqual(3, actionHandler.triggerCallsCount)
-//        XCTAssertEqual("a1", (actionsCalled[0] as! Action1).name)
-//        XCTAssertEqual("a2", (actionsCalled[1] as! Action2).name)
-//        XCTAssertEqual("a3", (actionsCalled[2] as! Action3).name)
-//    }
-//
-//    func testSideEffectMiddlewareUnknownEvent() {
-//        // Given
-//        let sut = SideEffectMiddlewareMock()
-//
-//        let actionHandler = ActionHandlerMock()
-//        sut.actionHandler = actionHandler
-//        let state = TestState()
-//        let getState = { state }
-//        let event = EventReference()
-//        let lastInChainWasCalledExpectation = self.expectation(description: "last in chain was called")
-//        let lastInChain = lastEventInChain(event, state: state, expectation: lastInChainWasCalledExpectation)
-//        let sideEffect = SideEffectProducerMock()
-//        sideEffect.executeGetStateReturnValue = observable(of: Action1(), Action2(), Action3())
-//        sut.sideEffectForClosure = { event in nil }
-//
-//        // Then
-//        sut.handle(event: event, getState: getState, next: lastInChain)
-//
-//        // Expect
-//        wait(for: [lastInChainWasCalledExpectation], timeout: 3)
-//        XCTAssertEqual(0, actionHandler.triggerCallsCount)
-//    }
-//
-//    func testSideEffectMiddlewareEventError() {
-//        // Given
-//        let sut = SideEffectMiddlewareMock()
-//
-//        let actionHandler = ActionHandlerMock()
-//        sut.actionHandler = actionHandler
-//        let state = TestState()
-//        let getState = { state }
-//        let event = EventReference()
-//        let lastInChainWasCalledExpectation = self.expectation(description: "last in chain was called")
-//        let lastInChain = lastEventInChain(event, state: state, expectation: lastInChainWasCalledExpectation)
-//        let sideEffect = SideEffectProducerMock()
-//        sideEffect.executeGetStateReturnValue =
-//            observable(of: Action1(), Action2())
-//                .concat(observable(of: ActionProtocol.self, error: SomeError()))
-//        sut.sideEffectForReturnValue = AnySideEffectProducer(sideEffect)
-//        sut.underlyingSubscriptionOwner = SubscriptionOwner.new()
-//        sut.underlyingAllowEventToPropagate = true
-//        var actionsCalled: [ActionProtocol] = []
-//        actionHandler.triggerClosure = { action in actionsCalled.append(action) }
-//
-//        // Then
-//        sut.handle(event: event, getState: getState, next: lastInChain)
-//
-//        // Expect
-//        wait(for: [lastInChainWasCalledExpectation], timeout: 3)
-//        XCTAssertEqual(3, actionHandler.triggerCallsCount)
-//        XCTAssertEqual("a1", (actionsCalled[0] as! Action1).name)
-//        XCTAssertEqual("a2", (actionsCalled[1] as! Action2).name)
-//        XCTAssertNotNil(actionsCalled[2] as? SideEffectError)
-//        XCTAssertNotNil((actionsCalled[2] as! SideEffectError).error)
-//        XCTAssertEqual(event, (actionsCalled[2] as! SideEffectError).originalEvent as! EventReference)
-//    }
+    func testSideEffectMiddlewareEvent() {
+        // Given
+        let sut = SideEffectMiddlewareMock<TestState>()
+
+        let messageHandler = MessageHandlerMock()
+        sut.handlers = messageHandler.value
+        let state = TestState()
+        let getState = { state }
+        let event = EventReference()
+        let lastInChainWasCalledExpectation = self.expectation(description: "last in chain was called")
+        let lastInChain = lastEventInChain(event, state: state, expectation: lastInChainWasCalledExpectation)
+        let sideEffect = SideEffectProducerMock<TestState>()
+        sideEffect.executeGetStateReturnValue = PublisherType(subscribe: { subscriber in
+            subscriber.onValue(Action1())
+            subscriber.onValue(Action2())
+            subscriber.onValue(Action3())
+            return FooSubscription()
+        })
+
+        sut.sideEffectForReturnValue = AnySideEffectProducer(sideEffect)
+        sut.underlyingAllowEventToPropagate = true
+
+        // Then
+        sut.handle(event: event, getState: getState, next: lastInChain)
+
+        // Expect
+        wait(for: [lastInChainWasCalledExpectation], timeout: 3)
+        XCTAssertEqual(3, messageHandler.actionHandlerMock.actions.count)
+        XCTAssertEqual("a1", (messageHandler.actionHandlerMock.actions[0] as! Action1).name)
+        XCTAssertEqual("a2", (messageHandler.actionHandlerMock.actions[1] as! Action2).name)
+        XCTAssertEqual("a3", (messageHandler.actionHandlerMock.actions[2] as! Action3).name)
+    }
+
+    func testSideEffectMiddlewareEventNoPropagation() {
+        // Given
+        let sut = SideEffectMiddlewareMock<TestState>()
+
+        let messageHandler = MessageHandlerMock()
+        sut.handlers = messageHandler.value
+        let state = TestState()
+        let getState = { state }
+        let event = EventReference()
+        let sideEffect = SideEffectProducerMock<TestState>()
+        sideEffect.executeGetStateReturnValue = PublisherType(subscribe: { subscriber in
+            subscriber.onValue(Action1())
+            subscriber.onValue(Action2())
+            subscriber.onValue(Action3())
+            return FooSubscription()
+        })
+        sut.sideEffectForReturnValue = AnySideEffectProducer(sideEffect)
+        sut.underlyingAllowEventToPropagate = false
+
+        // Then
+        sut.handle(event: event, getState: getState, next: { _, _ in XCTFail("It should be propagating") })
+
+        // Expect
+        XCTAssertEqual(3, messageHandler.actionHandlerMock.actions.count)
+        XCTAssertEqual("a1", (messageHandler.actionHandlerMock.actions[0] as! Action1).name)
+        XCTAssertEqual("a2", (messageHandler.actionHandlerMock.actions[1] as! Action2).name)
+        XCTAssertEqual("a3", (messageHandler.actionHandlerMock.actions[2] as! Action3).name)
+    }
+
+    func testSideEffectMiddlewareUnknownEvent() {
+        // Given
+        let sut = SideEffectMiddlewareMock<TestState>()
+
+        let messageHandler = MessageHandlerMock()
+        sut.handlers = messageHandler.value
+        let state = TestState()
+        let getState = { state }
+        let event = EventReference()
+        let lastInChainWasCalledExpectation = self.expectation(description: "last in chain was called")
+        let lastInChain = lastEventInChain(event, state: state, expectation: lastInChainWasCalledExpectation)
+        let sideEffect = SideEffectProducerMock<TestState>()
+        sideEffect.executeGetStateReturnValue = PublisherType(subscribe: { subscriber in
+            subscriber.onValue(Action1())
+            subscriber.onValue(Action2())
+            subscriber.onValue(Action3())
+            return FooSubscription()
+        })
+        sut.sideEffectForClosure = { event in nil }
+
+        // Then
+        sut.handle(event: event, getState: getState, next: lastInChain)
+
+        // Expect
+        wait(for: [lastInChainWasCalledExpectation], timeout: 3)
+        XCTAssertEqual(0, messageHandler.actionHandlerMock.actions.count)
+    }
+
+    func testSideEffectMiddlewareEventError() {
+        // Given
+        let sut = SideEffectMiddlewareMock<TestState>()
+
+        let messageHandler = MessageHandlerMock()
+        sut.handlers = messageHandler.value
+        let state = TestState()
+        let getState = { state }
+        let event = EventReference()
+        let lastInChainWasCalledExpectation = self.expectation(description: "last in chain was called")
+        let lastInChain = lastEventInChain(event, state: state, expectation: lastInChainWasCalledExpectation)
+        let sideEffect = SideEffectProducerMock<TestState>()
+        sideEffect.executeGetStateReturnValue = PublisherType(subscribe: { subscriber in
+            subscriber.onValue(Action1())
+            subscriber.onValue(Action2())
+            subscriber.onError(SomeError())
+            return FooSubscription()
+        })
+        sut.sideEffectForReturnValue = AnySideEffectProducer(sideEffect)
+        sut.underlyingAllowEventToPropagate = true
+
+        // Then
+        sut.handle(event: event, getState: getState, next: lastInChain)
+
+        // Expect
+        wait(for: [lastInChainWasCalledExpectation], timeout: 3)
+        XCTAssertEqual(3, messageHandler.actionHandlerMock.actions.count)
+        XCTAssertEqual("a1", (messageHandler.actionHandlerMock.actions[0] as! Action1).name)
+        XCTAssertEqual("a2", (messageHandler.actionHandlerMock.actions[1] as! Action2).name)
+        XCTAssertNotNil(messageHandler.actionHandlerMock.actions[2] as? SideEffectError)
+        XCTAssertNotNil((messageHandler.actionHandlerMock.actions[2] as! SideEffectError).error)
+        XCTAssertEqual(event, (messageHandler.actionHandlerMock.actions[2] as! SideEffectError).originalEvent as! EventReference)
+    }
 }
