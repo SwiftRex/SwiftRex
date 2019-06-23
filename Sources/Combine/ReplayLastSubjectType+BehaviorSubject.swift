812acb38b42bffc6f6de6b297111fc3f8201f8a9
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-06-23 23:34:18 +0200

    Combine bridge

---
+++ Sources/Combine/ReplayLastSubjectType+BehaviorSubject.swift
@@ -0,0 +1,23 @@
+import Combine
+import Foundation
+import SwiftRex
+
+extension ReplayLastSubjectType {
+    public init(currentValueSubject: CurrentValueSubject<Element, ErrorType>) {
+        self.init(
+            publisher: currentValueSubject.asPublisherType(),
+            subscriber: SubscriberType(
+                onValue: { currentValueSubject.value = $0 },
+                onCompleted: { error in
+                    currentValueSubject.send(completion: error.map(Subscribers.Completion<ErrorType>.failure) ?? .finished)
+                }
+            ),
+            value: { currentValueSubject.value }
+        )
+    }
+
+    public static func combine(initialValue: Element) -> ReplayLastSubjectType<Element, ErrorType> {
+        let currentValueSubject = CurrentValueSubject<Element, ErrorType>(initialValue)
+        return .init(currentValueSubject: currentValueSubject)
+    }
+}
---
+++ Sources/Combine/Subscription+Cancellable.swift
@@ -0,0 +1,41 @@
+import Combine
+import Foundation
+import SwiftRex
+
+extension SwiftRex.Subscription {
+    public func asCancellable() -> CancellableSubscription {
+        return CancellableSubscription(subscription: self)
+    }
+}
+
+public class CancellableSubscription: Cancellable, SwiftRex.Subscription, Combine.Subscription {
+    public func request(_ demand: Subscribers.Demand) {
+        // No support for backpressure
+    }
+
+    let cancellable: Cancellable
+
+    public init(cancellable: Cancellable) {
+        self.cancellable = cancellable
+    }
+
+    public init(subscription: SwiftRex.Subscription) {
+        self.cancellable = AnyCancellable {
+            subscription.unsubscribe()
+        }
+    }
+
+    public init(combineSubscription: Combine.Subscription) {
+        self.cancellable = AnyCancellable {
+            combineSubscription.cancel()
+        }
+    }
+
+    public func unsubscribe() {
+        cancellable.cancel()
+    }
+
+    public func cancel() {
+        cancellable.cancel()
+    }
+}
