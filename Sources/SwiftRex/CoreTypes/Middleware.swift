edfa8c69e4826e72c10a5e3be3c77a24db436cb8
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-09-22 22:50:06 +0200

    Update some docs

--- Sources/SwiftRex/CoreTypes/Middleware.swift
+++ Sources/SwiftRex/CoreTypes/Middleware.swift
@@ -1,9 +1,9 @@
 /**
- ⛓ `Middleware` is a plugin, or a composition of several plugins, that are assigned to the `Store` pipeline in order to handle each `EventProtocol` dispatched and to create `ActionProtocol` in response. It's also capable of handling each `ActionProtocol` before the `Reducer` to do its job.
+ ⛓ `Middleware` is a plugin, or a composition of several plugins, that are assigned to the `Store` pipeline in order to handle each `EventProtocol` dispatched and to create `ActionProtocol` in response. It's also capable of handling each `ActionProtocol` before the `Reducer` to do its job. So in other words, we can think of a Middleware as an object that transforms events into actions, running side-effects an async tasks in the process and able to check the current state at any point.
 
- We already know what `EventProtocol` and `ActionProtocol` are: both are lightweight structures that are dispatched (event) or triggered (action) into the `Store`. The store enqueues a new item that arrives and submits it to a pipeline of middlewares. So, in other words, a `Middleware` is class that handles actions and events, and has the power to trigger more actions to the `ActionHandler` chain. A very simple `Middleware` will receive an `EventProtocol` and current state, and trigger an equivalent `ActionProtocol` to the `Store`.
+ We already know what `EventProtocol` and `ActionProtocol` are both lightweight structures that are dispatched (event) or triggered (action) into the `Store`. The store enqueues a new item that arrives and submits it to a pipeline of middlewares. So, in other words, a `Middleware` is class that handles actions and events, and has the power to trigger more actions to the `ActionHandler` chain. A very simple `Middleware` will receive an `EventProtocol` and current state, and trigger an equivalent `ActionProtocol` to the `Store`.
 
- For example, a `MovieListEvent.didTapWatchToggle(rowIndex:)` event would be handled by a `Middleware` that checks the state and finds that current list has certain movie at the given row index, and this movie has been already marked as watched before. That `Middleware` was able to learn that thanks to the `getState: @escaping () -> StateType` closure received together with the `EventProtocol`, and which can be checked at any point to read an updated state. Our example `Middleware` then will trigger a `MovieListAction.setMovieAsUnwatched(movieId:)` action to be handled by itself, other middlewares or the `Reducer` chain.
+ For example, a `MovieListEvent.didTapWatchToggle(rowIndex:)` event would be handled by a `Middleware` that checks the state and finds that current list has certain movie at the given row index, and this movie has been already marked as watched before. The `Middleware` was able to learn that thanks to the `getState: @escaping () -> StateType` closure received together with the `EventProtocol`, and which can be checked at any point to read an updated state. Our example `Middleware` then will trigger a `MovieListAction.setMovieAsUnwatched(movieId:)` action to be handled by itself, other middlewares or the `Reducer` chain.
 
  However, this would be a very naive implementation. Most of the times we handle data that must be persisted in a database or through a REST API. State only lives in memory, but before changing the memory we should at least start Side-Effects. Let's revisit the example above and say that we got a `MovieListEvent.didTapWatchToggle(rowIndex:)` event. Yes, we still should check what movie is at that row and whether or not it's watched. Now, we can trigger a `URLSession` task requesting our API to mark it as unwatched. Because this request is asynchronous we have three options:
 
@@ -13,7 +13,7 @@
 
  In the first case, we create the `URLSessionDataTask`, call `task.resume()` and immediately trigger the `setMovieAsUnwatched` action. We may use the completion handler or the `HTTPRequest` to confirm the result and rollback if needed. In the second case, after calling `task.resume()` we don't trigger any action, only when we get the response in case it was successful one.
 
- The third case, however, offers many more possibilities. You can think about the possible three states of a movie: watched, not watched, mutating. You can even split the "mutating" case in two: "mutating to watched" and "mutating to unwatched". What you get from that is the ability to disable the "watch" button, or replaced it by an activity indicator view or simply ignore further attempts to click it by ignoring the events when the movie is in this intermediate situation. To offer that, you call task.resume and immediately trigger a `setMovieAsUnwatchRequestInProgress`, while inside the response completion handler you evaluate the response and trigger another action to update the movie state again.
+ The third case, however, offers many more possibilities. You can think about the possible three states of a movie: watched, not watched, mutating. You can even split the "mutating" case in two: "mutating to watched" and "mutating to unwatched". What you get from that is the ability to disable the "watch" button, or replaced it by an activity indicator view or simply ignore further attempts to click it by ignoring the events when the movie is in this intermediate situation. To offer that, you call `task.resume` and immediately trigger an Action `setMovieAsUnwatchRequestInProgress`, which will eventually set the state accordingly, while inside the response completion handler you evaluate the response and trigger another action to update the movie state again, by triggering either `setMovieAsUnwatched` in case of successful response, or back to `setMovieAsWatched` if the operation fails. In case of failure you may also consider to trigger an additional Action `gotError` so you notify the user, or maybe implement a retry.
 
  Because the `Middleware` accesses all events and the state of the app at any point, anything can be done in these small and reusable boxes. For example, the same `CoreLocation` middleware could be used from an iOS app, its extensions, the Apple Watch extension or even different apps, as long as they share some sub-state struct. Some suggestions of middlewares:
 
@@ -28,10 +28,13 @@
  - Analytics tracker
  - `WatchConnectivity` sync, keep iOS and watchOS state in sync
  - API calls and other "cold observables"
- - Reachability
+ - Network Reachability
  - Navigation through the app (Redux Coordinator pattern)
+ - `CoreBluetooth` central or peripheral manager
+ - `CoreNFC`
  - `NotificationCenter` and other delegates
- - `RxSwift` observables or `ReactiveSwift` signal producers
+ - `RxSwift` observables, `ReactiveSwift` signal producers, `Combine` publishers
+ - Any side-effect, I/O, networking, sensors, third-party libraries that you want to abstract
  */
 public protocol Middleware: class {
     /**
