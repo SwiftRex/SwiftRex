20fed368c3942546852862c87a10d978f9dae975
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-10-08 18:01:37 +0200

    Full integration test

--- Tests/SwiftRexTests/CoreTypes/StoreImplementations/ReduxStoreBaseTests.swift
+++ Tests/SwiftRexTests/CoreTypes/StoreImplementations/ReduxStoreBaseTests.swift
@@ -3,61 +3,101 @@ import Foundation
 import XCTest
 
 class ReduxStoreBaseTests: XCTestCase {
-    func testStoreFullWorkflow() { // swiftlint:disable:this function_body_length
-        let events: [EventProtocol] = [Event1(), Event3(), Event1(), Event2()]
+    // swiftlint:disable function_body_length
+    // swiftlint:disable cyclomatic_complexity
+    func testStoreFullWorkflowIntegration() {
+        let shouldCallFooMiddleware = expectation(description: "foo middleware should have been called 2 times")
+        shouldCallFooMiddleware.expectedFulfillmentCount = 2
+        let shouldCallBarMiddleware = expectation(description: "bar middleware should have been called 9 times")
+        shouldCallBarMiddleware.expectedFulfillmentCount = 9
+        let shouldCallReducer = expectation(description: "reducer should have been called 11 times")
+        shouldCallReducer.expectedFulfillmentCount = 11
+        shouldCallReducer.expectedFulfillmentCount = 11
+        let shouldNotifySubscribers = expectation(description: "subscription notification should have been called 11 times")
+        shouldNotifySubscribers.expectedFulfillmentCount = 11
+        let events: [AppAction] = [.foo, .bar(.charlie), .foo]
         let initialState = TestState()
-        let translateMiddleware = MiddlewareMock<TestState>()
-        translateMiddleware.handleEventGetStateNextClosure = { [weak translateMiddleware] event, getState, next in
-            next(event, getState)
-            XCTAssertEqual(initialState.value, getState().value)
-            var action: ActionProtocol?
-            switch event {
-            case is Event1: action = Action1()
-            case is Event2: action = Action2()
-            case is Event3: action = Action3()
-            default: XCTFail("Unexpected event")
+        let fooMiddleware = MiddlewareMock<AppAction, TestState>()
+        fooMiddleware.handleActionNextClosure = { [weak fooMiddleware] action, next in
+            guard action == .foo else {
+                next()
+                return
             }
-            action.map { translateMiddleware?.context().actionHandler.trigger($0) }
+
+            fooMiddleware?.context().dispatch(.bar(.alpha))
+            next()
+            fooMiddleware?.context().dispatch(.bar(.bravo))
+            shouldCallFooMiddleware.fulfill()
         }
-        translateMiddleware.handleActionGetStateNextClosure = { action, getState, next in
-            next(action, getState)
-            XCTAssertEqual(initialState.value, getState().value)
+        let barMiddleware = MiddlewareMock<AppAction.Bar, String>()
+        barMiddleware.handleActionNextClosure = { [weak barMiddleware] action, next in
+            switch action {
+            case .alpha:
+                DispatchQueue.global().asyncAfter(deadline: .now() + 0.15) {
+                    barMiddleware?.context().dispatch(.delta)
+                }
+            case .bravo:
+                DispatchQueue.global().asyncAfter(deadline: .now() + 0.5) {
+                    barMiddleware?.context().dispatch(.echo)
+                }
+            default: break
+            }
+            next()
+            shouldCallBarMiddleware.fulfill()
         }
+
         let subjectMock = CurrentValueSubject(currentValue: initialState)
-        let reducer: Reducer<TestState> = Reducer { state, event in
-            TestState(
+        let reducer: Reducer<AppAction, TestState> = Reducer { action, state in
+            let actionName: String
+            switch action {
+            case .foo: actionName = "foo"
+            case .bar(.alpha): actionName = "alpha"
+            case .bar(.bravo): actionName = "bravo"
+            case .bar(.charlie): actionName = "charlie"
+            case .bar(.delta): actionName = "delta"
+            case .bar(.echo): actionName = "echo"
+            }
+            shouldCallReducer.fulfill()
+            return .init(
                 value: state.value,
-                name: state.name + (
-                    (event as? Action1)?.name ??
-                        (event as? Action2)?.name ??
-                        (event as? Action3)?.name ??
-                    "not expected"
-                )
+                name: state.name + "_" + actionName
             )
         }
-        let store = TestStore(subject: subjectMock.subject,
-                              reducer: reducer,
-                              middleware: translateMiddleware)
+
+        let store = ReduxStoreBase<AppAction, TestState>(
+            subject: subjectMock.subject,
+            reducer: reducer,
+            middleware: fooMiddleware <> barMiddleware.lift(
+                actionZoomIn: { $0.bar },
+                actionZoomOut: { AppAction.bar($0) },
+                stateZoomIn: { $0.name }
+            )
+        )
 
         var count = 0
-        let shouldBeCalled4Times = expectation(description: "it should be called 4 times")
         _ = store.statePublisher.subscribe(SubscriberType(onValue: { value in
+            shouldNotifySubscribers.fulfill()
             switch count {
-            case 0: XCTAssertEqual("a1", value.name)
-            case 1: XCTAssertEqual("a1a3", value.name)
-            case 2: XCTAssertEqual("a1a3a1", value.name)
-            case 3:
-                XCTAssertEqual("a1a3a1a2", value.name)
-                shouldBeCalled4Times.fulfill()
+            case 0: XCTAssertEqual("_foo", value.name)
+            case 1: XCTAssertEqual("_foo_charlie", value.name)
+            case 2: XCTAssertEqual("_foo_charlie_foo", value.name)
+            case 3: XCTAssertEqual("_foo_charlie_foo_alpha", value.name)
+            case 4: XCTAssertEqual("_foo_charlie_foo_alpha_bravo", value.name)
+            case 5: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha", value.name)
+            case 6: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo", value.name)
+            case 7: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo_delta", value.name)
+            case 8: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo_delta_delta", value.name)
+            case 9: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo_delta_delta_echo", value.name)
+            case 10: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo_delta_delta_echo_echo", value.name)
             default: XCTFail("Called more times than expected")
             }
             count += 1
         }, onCompleted: { error in XCTFail("Unexpected completion. Error? \(String(describing: error))") }))
 
-        // Then
-        events.forEach(store.eventHandler.dispatch)
+        events.forEach(store.dispatch)
 
-        // Expect
-        wait(for: [shouldBeCalled4Times], timeout: 0.5)
+        waitForExpectations(timeout: 3)
+        XCTAssertEqual(subjectMock.currentValue.value, initialState.value)
+        XCTAssertEqual(subjectMock.currentValue.name, "_foo_charlie_foo_alpha_bravo_alpha_bravo_delta_delta_echo_echo")
     }
 }
--- Tests/SwiftRexTests/Mocks/Store/TestStore.swift
+++
@@ -1,9 +0,0 @@
-import SwiftRex
-
-final class TestStore: StoreBase<ActionMock, TestState> {
-    override init<M>(subject: UnfailableReplayLastSubjectType<StateType>,
-                     reducer: Reducer<ActionMock, StateType>,
-                     middleware: M) where ActionType == M.ActionType, StateType == M.StateType, M: Middleware {
-        super.init(subject: subject, reducer: reducer, middleware: middleware)
-    }
-}
