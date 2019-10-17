2c4458c893a7298542640abe715d69fd2d0ed861
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-10-17 23:59:37 +0200

    Temporarily remove everything from SPM that breaks SwiftUI Previews

--- Package.swift
+++ Package.swift
@@ -30,24 +30,14 @@ let package = Package(
         .watchOS(SupportedPlatform.WatchOSVersion.v6)
     ],
     products: combineProduct + [
-        .library(name: "ReactiveSwiftRex", targets: ["SwiftRex", "ReactiveSwiftRex"]),
         .library(name: "RxSwiftRex", targets: ["SwiftRex", "RxSwiftRex"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .exact("6.1.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", .exact("8.0.4"))
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
     ],
     targets: combineTargets + [
         .target(name: "SwiftRex", dependencies: []),
-        .target(name: "ReactiveSwiftRex", dependencies: ["SwiftRex", "ReactiveSwift"]),
-        .target(name: "RxSwiftRex", dependencies: ["SwiftRex", "RxSwift"]),
-        .testTarget(name: "SwiftRexTests",
-                    dependencies: ["SwiftRex", "Nimble"]),
-        .testTarget(name: "ReactiveSwiftRexTests",
-                    dependencies: ["SwiftRex", "ReactiveSwiftRex", "Nimble"]),
-        .testTarget(name: "RxSwiftRexTests",
-                    dependencies: ["SwiftRex", "RxSwiftRex", "Nimble", "RxBlocking", "RxTest"])
+        .target(name: "RxSwiftRex", dependencies: ["SwiftRex", "RxSwift"])
     ],
     swiftLanguageVersions: [.v5]
 )
