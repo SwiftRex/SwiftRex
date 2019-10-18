9c02efdcb15371b2a33bb70f4558eb99fdf676c1
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-10-19 00:19:32 +0200

    Rewrite test that protects against race condition inside the store

--- Tests/CombineRexTests/Integration/IssueTrackerTests39.swift
+++ Tests/CombineRexTests/Integration/IssueTrackerTests39.swift
@@ -6,133 +6,175 @@ import XCTest
 
 class IssueTracker39Tests: XCTestCase {
     struct MyState: Equatable, Codable {
-        var isPrepared: Bool
-        var isRunning: Bool
+        enum ExecutionState: Int, Equatable, Codable {
+            case stopped
+            case requested
+            case done
+        }
+
+        let preparation: ExecutionState
+        let running: ExecutionState
     }
 
-    enum MyEvent: EventProtocol, Equatable {
+    enum MyEvent: Equatable {
         case requestPrepare
         case requestRun
     }
 
-    enum MyAction: ActionProtocol, Equatable {
+    enum MyAction: Equatable {
         case prepare
         case run
     }
 
+    enum AppAction: Equatable {
+        case events(MyEvent)
+        case actions(MyAction)
+    }
+
     class MyMiddleware: Middleware {
-        var handlers: MessageHandler!
+        var context: (() -> MiddlewareContext<AppAction, MyState>) = { fatalError("Middleware context not set") }
 
-        func handle(event: EventProtocol, getState: @escaping GetState<MyState>, next: @escaping NextEventHandler<MyState>) {
-            next(event, getState)
+        func handle(action: AppAction, next: @escaping Next) {
+            switch action {
+            case .events(.requestPrepare):
+                // Nothing asked yet
+                XCTAssertEqual(context().getState().preparation, .stopped)
+                XCTAssertEqual(context().getState().running, .stopped)
 
-            switch event as? MyEvent {
-            case .requestPrepare?:
-                XCTAssertFalse(getState().isPrepared)
-                XCTAssertFalse(getState().isRunning)
+                next()
 
-                handlers.actionHandler.trigger(MyAction.prepare)
+                // After reducing "requestPrepare", preparation should have been requested
+                XCTAssertEqual(context().getState().preparation, .requested)
+                XCTAssertEqual(context().getState().running, .stopped)
 
-                XCTAssertTrue(getState().isPrepared)
-                XCTAssertFalse(getState().isRunning)
+                context().dispatch(.actions(.prepare))
 
-            case .requestRun?:
-                XCTAssertTrue(getState().isPrepared)
-                XCTAssertFalse(getState().isRunning)
+                // We expect the prepare action to happen only on the next runloop, so
+                // we still expect the state to remain unchanged
+                XCTAssertEqual(context().getState().preparation, .requested)
+                XCTAssertEqual(context().getState().running, .stopped)
 
-                handlers.actionHandler.trigger(MyAction.run)
+            case .events(.requestRun):
+                // Because request run was called immediately after request preparation,
+                // its execution will arrive at the store before "prepare" is ready, so it
+                // was only requested
+                XCTAssertEqual(context().getState().preparation, .requested)
+                XCTAssertEqual(context().getState().running, .stopped)
 
-                XCTAssertTrue(getState().isPrepared)
-                XCTAssertTrue(getState().isRunning)
+                if context().getState().preparation == .done && context().getState().running == .requested {
+                    context().dispatch(.actions(.run))
+                    // this should never happen, actually
+                    XCTFail("This should never happen")
+                }
 
-            default:
-                XCTFail("Unexpected event")
-            }
-        }
+                next()
 
-        func handle(action: ActionProtocol, getState: @escaping GetState<MyState>, next: @escaping NextActionHandler<MyState>) {
-            // Before reducing
-            switch action as? MyAction {
-            case .prepare?:
-                XCTAssertFalse(getState().isPrepared)
-                XCTAssertFalse(getState().isRunning)
+                // Both properties should be requested now, after reducing requestRun
+                XCTAssertEqual(context().getState().preparation, .requested)
+                XCTAssertEqual(context().getState().running, .requested)
+            case .actions(.prepare):
+                // Prepare is done, but we haven't reduced this yet, so state should be preparation
+                // requested. About running, it was requested before prepare was done, so both should
+                // be at requested state by now
+                XCTAssertEqual(context().getState().preparation, .requested)
+                XCTAssertEqual(context().getState().running, .requested)
 
-            case .run?:
-                XCTAssertTrue(getState().isPrepared)
-                XCTAssertFalse(getState().isRunning)
+                next()
 
-            default:
-                XCTFail("Unexpected action")
-            }
+                // After reducing "prepare", preparation should be done
+                XCTAssertEqual(context().getState().preparation, .done)
+                XCTAssertEqual(context().getState().running, .requested)
 
-            next(action, getState)
+                if context().getState().preparation == .done && context().getState().running == .requested {
+                    // We evaluate this same condition in two places because we can reach the pre-conditions
+                    // either when "preparation" or "running" states changed
+                    // This time we are expected to execute this operation
+                    context().dispatch(.actions(.run))
+                }
+            case .actions(.run):
+                // When we reach this point, preparation should be done and execution should have been
+                // requested, but not reduced yet
+                XCTAssertEqual(context().getState().preparation, .done)
+                XCTAssertEqual(context().getState().running, .requested)
 
-            // After reducing
-            switch action as? MyAction {
-            case .prepare?:
-                XCTAssertTrue(getState().isPrepared)
-                XCTAssertFalse(getState().isRunning)
+                next()
 
-            case .run?:
-                XCTAssertTrue(getState().isPrepared)
-                XCTAssertTrue(getState().isRunning)
-
-            default:
-                XCTFail("Unexpected action")
+                // Now everything should be done
+                XCTAssertEqual(context().getState().preparation, .done)
+                XCTAssertEqual(context().getState().running, .done)
             }
         }
     }
 
     var middleware: MyMiddleware!
-    var reducer: Reducer<MyState>!
-    var store: StoreBase<MyState>!
+    var reducer: Reducer<AppAction, MyState>!
+    var store: ReduxStoreBase<AppAction, MyState>!
 
     override func setUp() {
         super.setUp()
         middleware = MyMiddleware()
 
-        reducer = Reducer<MyState> { state, action in
-            switch action as? MyAction {
-            case .prepare?:
-                XCTAssertFalse(state.isPrepared)
-                XCTAssertFalse(state.isRunning)
-                return .init(isPrepared: true, isRunning: false)
-            case .run?:
-                XCTAssertTrue(state.isPrepared)
-                XCTAssertFalse(state.isRunning)
-                return .init(isPrepared: true, isRunning: true)
-            default:
-                XCTFail("Unexpected action")
+        reducer = Reducer<AppAction, MyState> { action, state in
+            switch action {
+            case .events(.requestPrepare):
+                XCTAssertEqual(state.preparation, .stopped)
+                XCTAssertEqual(state.running, .stopped)
+
+                return .init(preparation: .requested, running: state.running)
+            case .events(.requestRun):
+                XCTAssertEqual(state.preparation, .requested)
+                XCTAssertEqual(state.running, .stopped)
+
+                return .init(preparation: state.preparation, running: .requested)
+            case .actions(.prepare):
+                XCTAssertEqual(state.preparation, .requested)
+                XCTAssertEqual(state.running, .requested)
+
+                return .init(preparation: .done, running: state.running)
+            case .actions(.run):
+                XCTAssertEqual(state.preparation, .done)
+                XCTAssertEqual(state.running, .requested)
+
+                return .init(preparation: state.preparation, running: .done)
             }
-            return state
         }
 
-        store = StoreBase(
-            subject: .combine(initialValue: MyState(isPrepared: false, isRunning: false)),
+        store = ReduxStoreBase(
+            subject: .combine(initialValue: MyState(preparation: .stopped, running: .stopped)),
             reducer: reducer,
             middleware: middleware
         )
     }
 
-    func testIssue39() {
+    func testIssue39() { // swiftlint:disable:this function_body_length
         let shouldBeNotifiedAboutInitialState = expectation(description: "should be notified about initial state")
+        let shouldBeNotifiedAboutRequestedPrepare = expectation(description: "should be notified about requested prepare")
         let shouldBeNotifiedAboutPrepare = expectation(description: "should be notified about prepare")
+        let shouldBeNotifiedAboutRequestedRun = expectation(description: "should be notified about requested run")
         let shouldBeNotifiedAboutRun = expectation(description: "should be notified about run")
 
         var count = 0
         let subscription = store.statePublisher.sink { state in
             switch count {
             case 0:
-                XCTAssertFalse(state.isPrepared)
-                XCTAssertFalse(state.isRunning)
+                XCTAssertEqual(state.preparation, .stopped)
+                XCTAssertEqual(state.running, .stopped)
                 shouldBeNotifiedAboutInitialState.fulfill()
             case 1:
-                XCTAssertTrue(state.isPrepared)
-                XCTAssertFalse(state.isRunning)
-                shouldBeNotifiedAboutPrepare.fulfill()
+                XCTAssertEqual(state.preparation, .requested)
+                XCTAssertEqual(state.running, .stopped)
+                shouldBeNotifiedAboutRequestedPrepare.fulfill()
             case 2:
-                XCTAssertTrue(state.isPrepared)
-                XCTAssertTrue(state.isRunning)
+                XCTAssertEqual(state.preparation, .requested)
+                XCTAssertEqual(state.running, .requested)
+                shouldBeNotifiedAboutRequestedRun.fulfill()
+            case 3:
+                XCTAssertEqual(state.preparation, .done)
+                XCTAssertEqual(state.running, .requested)
+                shouldBeNotifiedAboutPrepare.fulfill()
+            case 4:
+                XCTAssertEqual(state.preparation, .done)
+                XCTAssertEqual(state.running, .done)
                 shouldBeNotifiedAboutRun.fulfill()
             default:
                 XCTFail("Unexpected notification")
@@ -140,10 +182,21 @@ class IssueTracker39Tests: XCTestCase {
             count += 1
         }
 
-        store.eventHandler.dispatch(MyEvent.requestPrepare)
-        store.eventHandler.dispatch(MyEvent.requestRun)
+        store.dispatch(.events(.requestPrepare))
+        store.dispatch(.events(.requestRun))
 
-        wait(for: [shouldBeNotifiedAboutInitialState, shouldBeNotifiedAboutPrepare, shouldBeNotifiedAboutRun], timeout: 2)
+        wait(
+            for: [
+                shouldBeNotifiedAboutInitialState,
+                shouldBeNotifiedAboutRequestedPrepare,
+                shouldBeNotifiedAboutRequestedRun,
+                shouldBeNotifiedAboutPrepare,
+                shouldBeNotifiedAboutRun
+            ],
+            timeout: 2,
+            enforceOrder: true
+        )
+
         XCTAssertNotNil(subscription)
     }
 }
