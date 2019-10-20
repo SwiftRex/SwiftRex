6cd4f396cd14083977e18a63b0f9a45fb4ef509e
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-10-20 16:38:58 +0200

    Remove duplicates on BindableStore

--- Sources/CombineRex/BindableStore.swift
+++ Sources/CombineRex/BindableStore.swift
@@ -43,15 +43,49 @@ public final class BindableStore<ViewAction, ViewState>: StoreType, ObservableOb
         viewStore.dispatch(action)
     }
 
-    public init(initialState: ViewState, viewStore: ViewStore<ViewAction, ViewState>) {
+    private init(initialState: ViewState,
+                 viewStore: ViewStore<ViewAction, ViewState>,
+                 removeDuplicates: @escaping (PublisherType<ViewState, Never>) -> Publishers.RemoveDuplicates<PublisherType<ViewState, Never>>) {
         self.state = initialState
         self.viewStore = viewStore
-        cancellableBinding = statePublisher.assign(to: \.state, on: self)
+        cancellableBinding = removeDuplicates(statePublisher).assign(to: \.state, on: self)
+    }
+
+    public convenience init(initialState: ViewState,
+                            viewStore: ViewStore<ViewAction, ViewState>,
+                            removeDuplicates: @escaping (ViewState, ViewState) -> Bool) {
+        self.init(initialState: initialState,
+                  viewStore: viewStore,
+                  removeDuplicates: { $0.removeDuplicates(by: removeDuplicates) })
+    }
+}
+
+extension BindableStore where ViewState: Equatable {
+    public convenience init(initialState: ViewState, viewStore: ViewStore<ViewAction, ViewState>) {
+        self.init(initialState: initialState,
+                  viewStore: viewStore,
+                  removeDuplicates: { $0.removeDuplicates() })
+
     }
 }
 
 extension StoreType {
     public func view<ViewAction, ViewState>(
+        action viewActionToGlobalAction: @escaping (ViewAction) -> ActionType?,
+        state globalStateToViewState: @escaping (StateType) -> ViewState,
+        initialState: ViewState,
+        removeDuplicates: @escaping (ViewState, ViewState) -> Bool) -> BindableStore<ViewAction, ViewState> {
+        let viewStore = self.view(
+            action: viewActionToGlobalAction,
+            state: { (globalStatePublisher: UnfailablePublisherType<StateType>) -> UnfailablePublisherType<ViewState> in
+                globalStatePublisher.map(globalStateToViewState).asPublisherType()
+            }
+        )
+
+        return .init(initialState: initialState, viewStore: viewStore, removeDuplicates: removeDuplicates)
+    }
+
+    public func view<ViewAction, ViewState: Equatable>(
         action viewActionToGlobalAction: @escaping (ViewAction) -> ActionType?,
         state globalStateToViewState: @escaping (StateType) -> ViewState,
         initialState: ViewState) -> BindableStore<ViewAction, ViewState> {
