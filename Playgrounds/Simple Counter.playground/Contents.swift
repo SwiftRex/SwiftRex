50f77e8d7bceaa82289552f54ffeaa69ddbdbba7
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-13 13:14:13 +0200

    Creates first playground

---
+++ Playgrounds/Simple Counter.playground/Contents.swift
@@ -0,0 +1,6 @@
+/*:
+# Simple Counter
+
+In this playground we're going to create a simple app that allows you to count things. It will have a text field showing the current count, as well as an "increment" and "decrement" button. Let's get started.
+ */
+
