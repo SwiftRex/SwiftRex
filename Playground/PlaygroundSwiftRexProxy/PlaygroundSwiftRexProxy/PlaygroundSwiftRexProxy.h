f1a68a3c0e5736fc213c3a31f0664c01ea54a700
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2020-06-13 15:26:51 +0200

    Playground workspace + counter

---
+++ Playground/PlaygroundSwiftRexProxy/PlaygroundSwiftRexProxy/PlaygroundSwiftRexProxy.h
@@ -0,0 +1,19 @@
+//
+//  PlaygroundSwiftRexProxy.h
+//  PlaygroundSwiftRexProxy
+//
+//  Created by Luiz Rodrigo Martins Barbosa on 13.06.20.
+//  Copyright © 2020 DeveloperCity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlaygroundSwiftRexProxy.
+FOUNDATION_EXPORT double PlaygroundSwiftRexProxyVersionNumber;
+
+//! Project version string for PlaygroundSwiftRexProxy.
+FOUNDATION_EXPORT const unsigned char PlaygroundSwiftRexProxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaygroundSwiftRexProxy/PublicHeader.h>
+
+
