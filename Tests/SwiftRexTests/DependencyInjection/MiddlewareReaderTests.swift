9be0f1faa3fd5d6853c9bcb03bd2749419501da2
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2020-06-21 00:31:45 +0200

    More reader tests

--- Tests/SwiftRexTests/DependencyInjection/MiddlewareReaderTests.swift
+++ Tests/SwiftRexTests/DependencyInjection/MiddlewareReaderTests.swift
@@ -20,4 +20,90 @@ class MiddlewareReaderTests: XCTestCase {
         wait(for: [expectsToCallInjection], timeout: 0)
         XCTAssert(middleware === resultMiddleware)
     }
+
+    func testMiddlewareReaderPure() {
+        // Given
+        let middleware = MiddlewareMock<String, String, String>()
+        let reader = MiddlewareReader<String, MiddlewareMock<String, String, String>>.pure(middleware)
+
+        // When
+        let resultMiddleware = reader.inject("Some environment")
+
+        // Then
+        XCTAssert(middleware === resultMiddleware)
+    }
+
+    func testMiddlewareReaderIdentityMiddlewareAction() {
+        // Given
+        let reader = MiddlewareReader<String, MiddlewareMock<AppAction, AppAction, TestState>>.identity
+        let sut = reader.inject("some")
+        var getStateCount = 0
+        var dispatchActionCount = 0
+
+        sut.receiveContext(
+            getState: {
+                getStateCount += 1
+                return TestState()
+            },
+            output: .init { _, _ in
+                dispatchActionCount += 1
+            })
+        let action = AppAction.bar(.delta)
+
+        // Then
+        var afterReducer: AfterReducer = .doNothing()
+        sut.handle(action: action, from: .here(), afterReducer: &afterReducer)
+
+        // Expect
+        let mirror = Mirror(reflecting: sut)
+        XCTAssertEqual("Mirror for IdentityMiddleware<AppAction, AppAction, TestState>", mirror.description)
+        XCTAssertEqual(0, dispatchActionCount)
+        XCTAssertEqual(0, getStateCount)
+    }
+
+    func testMiddlewareReaderMonoidIdentityMiddlewareAction() {
+        // Given
+        let reader = MiddlewareReader<String, MonoidMiddleware<AppAction, AppAction, TestState>>.identity
+        let sut = reader.inject("some")
+        var getStateCount = 0
+        var dispatchActionCount = 0
+
+        sut.receiveContext(
+            getState: {
+                getStateCount += 1
+                return TestState()
+            },
+            output: .init { _, _ in
+                dispatchActionCount += 1
+            })
+        let action = AppAction.bar(.delta)
+
+        // Then
+        var afterReducer: AfterReducer = .doNothing()
+        sut.handle(action: action, from: .here(), afterReducer: &afterReducer)
+
+        // Expect
+        let mirror = Mirror(reflecting: sut)
+        XCTAssertEqual("Mirror for MonoidMiddleware<AppAction, AppAction, TestState>", mirror.description)
+        XCTAssertEqual(0, dispatchActionCount)
+        XCTAssertEqual(0, getStateCount)
+    }
+}
+
+struct MonoidMiddleware<InputActionType, OutputActionType, StateType>: Middleware, Monoid {
+    func receiveContext(getState: @escaping GetState<StateType>, output: AnyActionHandler<OutputActionType>) {
+    }
+
+    func handle(action: InputActionType, from dispatcher: ActionSource, afterReducer: inout AfterReducer) {
+    }
+
+    let string: String
+
+    static var identity: MonoidMiddleware {
+        .init(string: "")
+    }
+
+    static func <> (lhs: MonoidMiddleware, rhs: MonoidMiddleware) -> MonoidMiddleware {
+        .init(string: lhs.string + rhs.string)
+    }
 }
