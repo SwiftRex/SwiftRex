2ab538a574be480024780de8c547ca3265e7299e
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2022-01-07 17:51:26 +0100

    More DispatchQueue tests

--- Tests/SwiftRexTests/Foundation/DispatchQueueTests.swift
+++ Tests/SwiftRexTests/Foundation/DispatchQueueTests.swift
@@ -61,6 +61,26 @@ class DispatchQueueTests: XCTestCase {
         wait(for: [call1, call2], timeout: 0.1, enforceOrder: true)
     }
 
+    func testDispatchAsapWithOptionsOnMainQueueAlready() {
+        DispatchQueue.setMainQueueID()
+
+        XCTAssert(DispatchQueue.isMainQueue)
+        XCTAssert(Thread.isMainThread)
+        let call1 = expectation(description: "1")
+        let call2 = expectation(description: "2")
+
+        DispatchQueue.asap(options: .init(qos: .background, flags: .detached, group: nil)) {
+            XCTAssert(DispatchQueue.isMainQueue)
+            XCTAssert(Thread.isMainThread)
+            call1.fulfill()
+        }
+        XCTAssert(DispatchQueue.isMainQueue)
+        XCTAssert(Thread.isMainThread)
+        call2.fulfill()
+
+        wait(for: [call1, call2], timeout: 0.1, enforceOrder: true)
+    }
+
     func testDispatchAsapOnMainQueueAlreadyDispatched() {
         let call1 = expectation(description: "1")
         let call2 = expectation(description: "2")
@@ -72,6 +92,29 @@ class DispatchQueueTests: XCTestCase {
             XCTAssert(Thread.isMainThread)
 
             DispatchQueue.asap {
+                XCTAssert(DispatchQueue.isMainQueue)
+                XCTAssert(Thread.isMainThread)
+                call1.fulfill()
+            }
+            XCTAssert(DispatchQueue.isMainQueue)
+            XCTAssert(Thread.isMainThread)
+            call2.fulfill()
+        }
+
+        wait(for: [call1, call2], timeout: 0.1, enforceOrder: true)
+    }
+
+    func testDispatchAsapWithOptionsOnMainQueueAlreadyDispatched() {
+        let call1 = expectation(description: "1")
+        let call2 = expectation(description: "2")
+
+        DispatchQueue.main.async {
+            DispatchQueue.setMainQueueID()
+
+            XCTAssert(DispatchQueue.isMainQueue)
+            XCTAssert(Thread.isMainThread)
+
+            DispatchQueue.asap(options: .init(qos: .background, flags: .detached, group: nil)) {
                 XCTAssert(DispatchQueue.isMainQueue)
                 XCTAssert(Thread.isMainThread)
                 call1.fulfill()
@@ -96,6 +139,31 @@ class DispatchQueueTests: XCTestCase {
                 XCTAssertFalse(Thread.isMainThread)
 
                 DispatchQueue.asap {
+                    XCTAssert(DispatchQueue.isMainQueue)
+                    XCTAssert(Thread.isMainThread)
+                    call1.fulfill()
+                }
+                XCTAssertFalse(DispatchQueue.isMainQueue)
+                XCTAssertFalse(Thread.isMainThread)
+                call2.fulfill()
+            }
+        }
+
+        wait(for: [call2, call1], timeout: 0.1, enforceOrder: true)
+    }
+
+    func testDispatchAsapWithOptionsFromGlobalQueue() {
+        let call1 = expectation(description: "1")
+        let call2 = expectation(description: "2")
+
+        DispatchQueue.main.async {
+            DispatchQueue.setMainQueueID()
+
+            DispatchQueue.global().async {
+                XCTAssertFalse(DispatchQueue.isMainQueue)
+                XCTAssertFalse(Thread.isMainThread)
+
+                DispatchQueue.asap(options: .init(qos: .background, flags: .detached, group: nil)) {
                     XCTAssert(DispatchQueue.isMainQueue)
                     XCTAssert(Thread.isMainThread)
                     call1.fulfill()
