adf19049e711a808d230d63ee2643f02cecd755f
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2022-02-17 13:59:46 +0100

    Remove ASAPScheduler redundant conformance (#125) (#126)
    * Remove ASAPScheduler redundant conformance (#125)
    
    * Update Mintfile
    
    * Revert code-gen

--- Tests/SwiftRexTests/CodeGeneration/AutoMockable.generated.swift
+++ Tests/SwiftRexTests/CodeGeneration/AutoMockable.generated.swift
@@ -1,6 +1,5 @@
-// Generated using Sourcery 0.18.0 — https://github.com/krzysztofzablocki/Sourcery
+// Generated using Sourcery 1.6.1 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
-
 // swiftlint:disable all
 import Foundation
 @testable import SwiftRex
