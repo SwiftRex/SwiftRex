9e0a6047e26da498b085b69f685d12f174d36c3b
Hai Feng Kao <khaifeng@gmail.com>
2025-03-23 23:27:32 +0800

    change 1 test expected behavior

--- Tests/SwiftRexTests/CoreTypes/StoreImplementations/ReduxStoreBaseTests.swift
+++ Tests/SwiftRexTests/CoreTypes/StoreImplementations/ReduxStoreBaseTests.swift
@@ -9,18 +9,16 @@ class ReduxStoreBaseTests: XCTestCase {
         let shouldCallFooMiddleware = expectation(description: "foo middleware should have been called 2 times")
         shouldCallFooMiddleware.expectedFulfillmentCount = 2
         let shouldCallBarMiddleware = expectation(description: "bar middleware should have been called 9 times")
-        shouldCallBarMiddleware.expectedFulfillmentCount = 9
+        shouldCallBarMiddleware.expectedFulfillmentCount = 7
         let shouldCallReducer = expectation(description: "reducer should have been called 11 times")
-        shouldCallReducer.expectedFulfillmentCount = 11
+        shouldCallReducer.expectedFulfillmentCount = 9
         let shouldNotifySubscribers = expectation(description: "subscription notification should have been called 11 times")
-        shouldNotifySubscribers.expectedFulfillmentCount = 11
+        shouldNotifySubscribers.expectedFulfillmentCount = 9
         let events: [AppAction] = [.foo, .bar(.charlie), .foo]
         let initialState = TestState()
         let fooMiddleware = IsoMiddlewareMock<AppAction, TestState>()
         var fooMiddlewareOutput: AnyActionHandler<AppAction>?
-        fooMiddleware.receiveContextGetStateOutputClosure = { _, output in
-            fooMiddlewareOutput = output
-        }
+      
         fooMiddleware.handleActionFromStateClosure = { action, _, _ in
             guard action == .foo else {
                 return .pure()
@@ -28,16 +26,15 @@ class ReduxStoreBaseTests: XCTestCase {
 
             fooMiddlewareOutput?.dispatch(.bar(.alpha), from: .here())
 
-            return IO { _ in
-                fooMiddlewareOutput?.dispatch(.bar(.bravo), from: .here())
+            return IO { output in
+                fooMiddlewareOutput = output
+                output.dispatch(.bar(.bravo), from: .here())
                 shouldCallFooMiddleware.fulfill()
             }
         }
         let barMiddleware = IsoMiddlewareMock<AppAction.Bar, String>()
         var barMiddlewareOutput: AnyActionHandler<AppAction.Bar>?
-        barMiddleware.receiveContextGetStateOutputClosure = { _, output in
-            barMiddlewareOutput = output
-        }
+       
         barMiddleware.handleActionFromStateClosure = { action, _, _ in
             switch action {
             case .alpha:
@@ -51,7 +48,8 @@ class ReduxStoreBaseTests: XCTestCase {
             default: break
             }
 
-            return IO { _ in
+            return IO { output in
+                barMiddlewareOutput = output
                 shouldCallBarMiddleware.fulfill()
             }
         }
@@ -81,22 +79,22 @@ class ReduxStoreBaseTests: XCTestCase {
                 state: { $0.name }
             )
         )
-
+        
+        // receiveContext uses DispatchQueue.main.async, but handle uses DispatchQueue.asap
+        // the behavior is different
         var count = 0
         _ = store.statePublisher.subscribe(SubscriberType(onValue: { value in
             shouldNotifySubscribers.fulfill()
             switch count {
             case 0: XCTAssertEqual("_foo", value.name)
-            case 1: XCTAssertEqual("_foo_charlie", value.name)
-            case 2: XCTAssertEqual("_foo_charlie_foo", value.name)
-            case 3: XCTAssertEqual("_foo_charlie_foo_alpha", value.name)
-            case 4: XCTAssertEqual("_foo_charlie_foo_alpha_bravo", value.name)
-            case 5: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha", value.name)
-            case 6: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo", value.name)
-            case 7: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo_delta", value.name)
-            case 8: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo_delta_delta", value.name)
-            case 9: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo_delta_delta_echo", value.name)
-            case 10: XCTAssertEqual("_foo_charlie_foo_alpha_bravo_alpha_bravo_delta_delta_echo_echo", value.name)
+            case 1: XCTAssertEqual("_foo_bravo", value.name)
+            case 2: XCTAssertEqual("_foo_bravo_charlie", value.name)
+            case 3: XCTAssertEqual("_foo_bravo_charlie_alpha", value.name)
+            case 4: XCTAssertEqual("_foo_bravo_charlie_alpha_foo", value.name)
+            case 5: XCTAssertEqual("_foo_bravo_charlie_alpha_foo_bravo", value.name)
+            case 6: XCTAssertEqual("_foo_bravo_charlie_alpha_foo_bravo_delta", value.name)
+            case 7: XCTAssertEqual("_foo_bravo_charlie_alpha_foo_bravo_delta_echo", value.name)
+            case 8: XCTAssertEqual("_foo_bravo_charlie_alpha_foo_bravo_delta_echo_echo", value.name)
             default: XCTFail("Called more times than expected")
             }
             count += 1
@@ -106,6 +104,6 @@ class ReduxStoreBaseTests: XCTestCase {
 
         waitForExpectations(timeout: 5)
         XCTAssertEqual(subjectMock.currentValue.value, initialState.value)
-        XCTAssertEqual(subjectMock.currentValue.name, "_foo_charlie_foo_alpha_bravo_alpha_bravo_delta_delta_echo_echo")
+        XCTAssertEqual(subjectMock.currentValue.name, "_foo_bravo_charlie_alpha_foo_bravo_delta_echo_echo")
     }
 }
